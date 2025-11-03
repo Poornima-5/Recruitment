6c7d84448af3678977c0edf7776763435290f667
Poornima-5 <poornimaudayamoorthy@gmail.com>
2025-11-04 01:36:12 +0530

    Remove supabase key logs

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -3,7 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
@@ -14,6 +13,3 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     autoRefreshToken: true,
   },
 });
-    ;
-console.log("URL:", SUPABASE_URL);
-console.log("KEY:", SUPABASE_ANON_KEY);
